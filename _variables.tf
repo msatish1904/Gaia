97b8ea8b00c14cb2f015beef2ebd05eeaa206427
msatish1904 <99471611+msatish1904@users.noreply.github.com>
2024-02-19 21:28:19 +0530

    Update _variables.tf

--- _variables.tf
+++ _variables.tf
@@ -1,3 +1,47 @@
+
+variable "instance-tag-name" {
+  description = "instance-tag-name"
+  type        = "string"
+  default     = "EC2-instance-created-with-terraform"
+}
+
+variable "vpc-cidr-block" {
+  description = "The CIDR block to associate to the VPC"
+  type        = "string"
+  default     = "10.0.0.0/16"
+}
+
+variable "subnet-cidr-block" {
+  description = "The CIDR block to associate to the subnet"
+  type        = "string"
+  default     = "10.0.1.0/24"
+}
+
+variable "vpc-tag-name" {
+  description = "The Name to apply to the VPC"
+  type        = "string"
+  default     = "VPC-created-with-terraform"
+}
+
+variable "ig-tag-name" {
+  description = "The name to apply to the Internet gateway tag"
+  type        = "string"
+  default     = "aws-ig-created-with-terraform"
+}
+
+variable "subnet-tag-name" {
+  description = "The Name to apply to the VPN"
+  type        = "string"
+  default     = "VPN-created-with-terraform"
+}
+
+variable "sg-tag-name" {
+  description = "The Name to apply to the security group"
+  type        = "string"
+  default     = "SG-created-with-terraform"
+}
+
+
 variable "aws-region" {
   description = "The AWS region"
   type        = "string"
@@ -44,44 +88,4 @@ variable "user-data-script" {
   default     = ""
 }
 
-variable "instance-tag-name" {
-  description = "instance-tag-name"
-  type        = "string"
-  default     = "EC2-instance-created-with-terraform"
-}
 
-variable "vpc-cidr-block" {
-  description = "The CIDR block to associate to the VPC"
-  type        = "string"
-  default     = "10.0.0.0/16"
-}
-
-variable "subnet-cidr-block" {
-  description = "The CIDR block to associate to the subnet"
-  type        = "string"
-  default     = "10.0.1.0/24"
-}
-
-variable "vpc-tag-name" {
-  description = "The Name to apply to the VPC"
-  type        = "string"
-  default     = "VPC-created-with-terraform"
-}
-
-variable "ig-tag-name" {
-  description = "The name to apply to the Internet gateway tag"
-  type        = "string"
-  default     = "aws-ig-created-with-terraform"
-}
-
-variable "subnet-tag-name" {
-  description = "The Name to apply to the VPN"
-  type        = "string"
-  default     = "VPN-created-with-terraform"
-}
-
-variable "sg-tag-name" {
-  description = "The Name to apply to the security group"
-  type        = "string"
-  default     = "SG-created-with-terraform"
-}
